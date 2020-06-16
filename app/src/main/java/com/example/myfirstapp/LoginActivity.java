fb07bd6e341ad36e7c011efc5f34c0be4040fda2
lukjack92 <lukjack92@gmail.com>
2020-06-17 01:28:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapp/LoginActivity.java
@@ -0,0 +1,125 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_ui);
+    }
+
+    public void submitLogin(View view) throws Exception {
+        EditText usernameView = findViewById(R.id.loginUsername);
+        EditText passwordView = findViewById(R.id.loginPassword);
+
+        String username = usernameView.getText().toString().trim();
+        String password = passwordView.getText().toString().trim();
+
+        if(username.length() == 0 || password.length() == 0) {
+            Toast.makeText(getApplicationContext(),"Entry Username and Password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+/*
+        JSONObject loginForm = new JSONObject();
+
+        try {
+            loginForm.put("type", "login");
+            loginForm.put("email", username);
+            loginForm.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+   */
+        RequestParams params = new RequestParams();
+        params.add("type","login");
+        params.add("email",username);
+        params.add("password",password);
+
+        RequestBody body = RequestBody.create(MediaType.parse("application/x-www-form-urlencoded; charset=utf-8"), params.toString());
+        postRequest(MainActivity.postURI, body);
+    }
+
+    public void postRequest(String postUrl, RequestBody postBody) {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(postUrl)
+                .post(postBody)
+                //.header("Accept", "application/x-www-form-urlencoded")
+                //.header("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // Cancel the post on failure.
+                call.cancel();
+                Log.d("FAIL", e.getMessage());
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView responseTextLogin = findViewById(R.id.responseTextLogin);
+                        responseTextLogin.setText("Server is unreachable. Please try soon.");
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView responseTextLogin = findViewById(R.id.responseTextLogin);
+                        try {
+                            String loginResponseString = response.body().string();
+                            //JSONObject user = response.getJSONObject("user");
+                            JSONObject jsonResponse = new JSONObject(loginResponseString);
+                            Log.d("Message", "Message form the server : " + jsonResponse.getString("message").trim());
+                            Log.d("LOGIN", "Response from the server : " + loginResponseString);
+                            if (jsonResponse.getString("message").trim().equals("Successfully logged in.")) {
+                                Log.d("LOGIN", "Successful Login");
+                                setContentView(R.layout.activity_display_message);
+                                TextView responseTextLoginToScreen = findViewById(R.id.textView);
+                                responseTextLoginToScreen.setText(jsonResponse.getString("message").trim());
+                                //finish();//finishing activity and return to the calling activity.
+                            } else if (jsonResponse.getString("message").trim().equals("User not found or Invalid login details.")) {
+                                responseTextLogin.setText("Login Failed. Invalid username or password.");
+                                Log.d("LOGIN", "Unsuccessful Login");
+                            } else {
+                                //responseTextLogin.setText(loginResponseString);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            responseTextLogin.setText("The page is unreachable. Please try soon.");
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.myfirstapp;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static final String postURI = "https://mgr.ljack.com.pl/api/api.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_login_ui);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 0);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
